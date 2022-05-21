f748845bdf045a0a4ec5b6a0d328d11b4828827f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-21 16:20:24 -0300

    Update marina.ts

--- src/utils/marina.ts
+++ src/utils/marina.ts
@@ -1,19 +1,8 @@
-import { MarinaProvider } from 'marina-provider';
+import { detectProvider } from 'marina-provider';
 
-const MAX_TRY = 60;
-const INTERVAL_MS = 1000;
-
-export async function getMarina(): Promise<MarinaProvider> {
-	for (let i = 0; i < MAX_TRY; i++) {
-		const isInstalled = typeof (window as any).marina !== "undefined";
-		if (!isInstalled) {
-			await new Promise<void>((resolve) => setTimeout(() => resolve(), INTERVAL_MS))
-			continue;
-		}
-
-		const marina: MarinaProvider = (window as any).marina;
-		return marina;
-	}
-
-	throw new Error("Marina needs to be installed");
+try {
+  const marina = await detectProvider('marina');
+  initApp(marina); // initialize your app
+} catch (err) {
+  console.log('Please install Marina extension!');
 }
