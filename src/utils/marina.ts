5d207e32235661ade58e182039269df1fa0be35a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-21 16:22:42 -0300

    Delete marina.ts

--- src/utils/marina.ts
+++
@@ -1,8 +0,0 @@
-import { detectProvider } from 'marina-provider';
-
-try {
-  const marina = await detectProvider('marina');
-  initApp(marina); // initialize your app
-} catch (err) {
-  console.log('Please install Marina extension!');
-}
